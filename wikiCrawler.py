99fc75992f2d02dbf47b088717ab43e85ef58fb4
adogo006 <peand006@gmail.com>
2026-03-06 14:28:05 +0900

    second commit

---
+++ wikiCrawler.py
@@ -0,0 +1,16 @@
+from urllib.request import urlopen
+from urllib.request import HTTPError
+from urllib.request import URLError
+import re
+
+from bs4 import BeautifulSoup
+
+
+
+
+
+def main():
+
+if __name__ == "__main__":
+    main()
+
